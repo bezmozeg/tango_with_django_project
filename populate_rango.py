de5103659d90be48b8e94efdd400da5b6bc74f1e
Richard Nemeth <2333065n@student.gla.ac.uk>
2019-01-27 13:07:39 +0000

    finished chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -1,4 +1,5 @@
 import os
+import random
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                     'tango_with_django_project.settings')
 
@@ -7,6 +8,7 @@ django.setup()
 from rango.models import Category,Page
 
 def populate():
+
     python_pages = [
 {"title": "Official Python Tutorial",
 "url":"http://docs.python.org/2/tutorial/"},
@@ -29,29 +31,33 @@ def populate():
 {"title":"Flask",
 "url":"http://flask.pocoo.org"} ]
 
-cats = {"Python": {"pages": python_pages},
-"Django": {"pages": django_pages},
-"Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages,'views':128,'likes':64},
+"Django": {"pages": django_pages,'views':64, 'likes':32},
+"Other Frameworks": {"pages": other_pages,'views':32, 'likes':16} }
 
-for cat,cat_data in cats:
-    c = add_cat(cat)
-    for p in cat_data:
-        add_page(c,p['title'],p['url'])
+    for cat,cat_data in cats.items():
+        c = add_cat(cat,cat_data['views'],cat_data['likes'])
+        for p in cat_data["pages"]:
+            add_page(c,p['title'],p['url'])
 
-for c in Category.objects.all():
-    for p in Page.Objects.filter(category = c):
-        print("- {0} - {1}".format(str(c), str(p)))
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category = c):
+            print("- {0} - {1}".format(str(c), str(p)))
 
 def add_page(cat, title, url, views=0):
+    views = random.randint(10,120)
     p = Page.objects.get_or_create(category=cat,title=title)[0]
     p.url = url
     p.views= views
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name,views=0,likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
+
     return c
 
 if __name__ == '__main__':
--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
 
     name = models.CharField(max_length=128,unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(default='',unique=True)
+
+    def save(self, *args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Category,self).save(*args,**kwargs)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -17,5 +25,7 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default =0)
 
+    def __str__(self):
+        return self.title
 
 # Create your models here.
