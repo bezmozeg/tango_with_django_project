1b500d74273ffba76be58bf26858dba027848bdc
Richard Nemeth <2333065n@student.gla.ac.uk>
2019-02-08 17:36:43 +0000

    chapter 9 and 10

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,11 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from django.http import HttpResponseRedirect
 from rango.forms import PageForm
+from rango.forms import UserProfileForm, UserForm
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 
 def index(request):
@@ -78,3 +83,81 @@ def add_page(request,category_name_slug):
                 print(form.errors)
     conext = {'form': form, 'category': category}
     return render(request,'rango/add_page.html',conext)
+
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        user_form = UserForm(data = request.POST)
+        profile_form = UserProfileForm(data = request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit = False)
+
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
+
+
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+        'rango/register.html',
+        {'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered})
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+
+            if user.is_active:
+
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("Your Rango account is disabled")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+
+        return render(request,'rango/login.html')
+
+@login_required
+def restricted(request):
+    return render(request,'rango/restricted.html',{})
+
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return HttpResponseRedirect(reverse('index'))
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
+LOGIN_URL = '/rango/login/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -98,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
